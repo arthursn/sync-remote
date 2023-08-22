d9025526cfdde56187db5f953b7edbd4dfcded23
Arthur Nishikawa <nishikawa.poli@gmail.com>
2023-08-22 16:47:11 +0200

    First commit

---
+++ sync-remote.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+function dirnames {
+    local input
+    local parent
+    input="$1"
+    if [[ "$input" == "" ]]; then
+        true
+    elif [[ "$input" == "." || "$input" == "/" ]]; then
+        printf '%s\n' "$input" #print the root node
+    else
+        #printf '%s\n' "$input"  #print node (descending)
+        parent="$(dirname "$input")"
+        dirnames "$parent"
+        printf '%s\n' "$input" #print node (ascending)
+    fi
+}
+
+make_include_string() {
+    include_str=""
+    dirs=()
+    for f in $@; do
+        for d in $(dirnames $(dirname $f)); do
+            if [[ ! " ${dirs[*]} " =~ " ${d} " ]]; then
+                include_str="$include_str --include=$d/"
+                dirs="${dirs[@]} $d"
+            fi
+        done
+        include_str="$include_str --include=$f"
+    done
+    echo $include_str
+}
+
+sync() {
+    # Parse remote path (remote:remote_dir)
+    IFS=':' read -ra arr <<<"$2"
+    remote="${arr[0]}"
+    remote_dir="${arr[1]}"
+    # Check if variables are not empty
+    [[ -z $remote_dir || -z $remote_dir ]] && usage
+
+    case $1 in
+    push)
+        # Include all changed files that are not deleted
+        files=$(git status --porcelain | awk '!match($1, "D"){print $2}')
+        include_str=$(make_include_string $files)
+        (
+            set -x
+            rsync -arvP $include_str --exclude=".*" --exclude="*" . $remote:$remote_dir/
+        )
+        ;;
+    pull)
+        # Include all changed files that are not deleted
+        files=$(ssh $remote "git -C $remote_dir status --porcelain" | awk '!match($1, "D"){print $2}')
+        include_str=$(make_include_string $files)
+        (
+            set -x
+            rsync -arvP $include_str --exclude=".*" --exclude="*" $remote:$remote_dir/ .
+        )
+        ;;
+    *)
+        echo "Invalid option $1"
+        exit 1
+        ;;
+    esac
+}
+
+usage() {
+    echo "Usage: $0 [push pull] [remote:remote_dir]"
+    exit 1
+}
+
+case $1 in
+push)
+    sync $@
+    exit $?
+    ;;
+pull)
+    sync $@
+    exit $?
+    ;;
+install)
+    usage
+    ;;
+update)
+    usage
+    ;;
+*)
+    usage
+    ;;
+esac
